3a7c55c9bde1e464052c32349cb6841813e13e96
Clifton Hensley <chensley@slack-corp.com>
2021-09-16 11:27:02 -0500

    Adding parameter to optionally tag the previous sha

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # GitHub Update Tag Action
-A GitHub action that simply tags the repository with the specified tag. If the tag exists it gets updated.
+A GitHub action that simply tags the repository with the specified tag. If the tag exists it gets updated. Optionally marks the specified tag's current sha before updating. 
 
 ## Usage
 ```yml
@@ -15,6 +15,7 @@ jobs:
         uses: richardsimko/update-tag@v1
         with:
           tag_name: name-of-tag
+          previous_tag_name: name-of-tag-previous
         env:
           GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
 ```
@@ -22,3 +23,4 @@ jobs:
 ### Inputs
 
 - **tag_name** _(required)_ - The name of the tag you want to create or update.
+- **previous_tag_name** _(optional)_ - The name of the tag referencing the tag's previous sha. 
--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ async function run() {
   try {
     const { GITHUB_SHA, GITHUB_TOKEN } = process.env;
     const tagName = core.getInput('tag_name');
+    const previousTagName = core.getInput('previous_tag_name');
+
     if (!GITHUB_SHA) {
       core.setFailed('Missing GITHUB_SHA');
       return;
@@ -20,35 +22,63 @@ async function run() {
       return;
     }
 
-
     const octokit = new GitHub(GITHUB_TOKEN);
-    let ref;
-    try {
 
-      ref = await octokit.git.getRef({
-        ...context.repo,
-        ref: `tags/${tagName}`
-      });
-    } catch (e) {
-      if (e.status === 404) {
-        // Ignore tag not existing
-      } else {
-        throw e;
+    async function getRef(tag) {
+      try {
+        return await octokit.git.getRef({
+          ...context.repo,
+          ref: `tags/${tag}`
+        });
+      } catch (e) {
+        if (e.status === 404) {
+          // Ignore tag not existing
+        } else {
+          throw e;
+        }
       }
     }
-    if (!ref) {
-      await octokit.git.createRef({
+
+    // Get the existing tag reference
+    const tagRef = await getRef(tagName);
+
+    // If there is no existing reference, create one and exit
+    if (!tagRef) {
+      return await octokit.git.createRef({
         ...context.repo,
         ref: `refs/tags/${tagName}`,
         sha: GITHUB_SHA
       });
-    } else {
-      await octokit.git.updateRef({
-        ...context.repo,
-        ref: `tags/${tagName}`,
-        sha: GITHUB_SHA
-      });
     }
+
+    // Get the sha of the existing tag
+    const { data: { object: { sha } = {}} = {}} = tagRef;
+
+    // If previous_tag_name is provided, create or update it at the current tag's sha
+    if (previousTagName) {
+      const previousTagRef = await getRef(previousTagName);
+
+      if (!previousTagRef) {
+        await octokit.git.createRef({
+          ...context.repo,
+          ref: `refs/tags/${previousTagName}`,
+          sha
+        });
+      } else {
+        await octokit.git.updateRef({
+          ...context.repo,
+          ref: `tags/${previousTagName}`,
+          sha
+        });
+      }
+    }
+
+    // Finally, update the tag to the GITHUB_SHA
+    await octokit.git.updateRef({
+      ...context.repo,
+      ref: `tags/${tagName}`,
+      sha: GITHUB_SHA
+    });
 
   } catch (error) {
     core.setFailed(error.message);
